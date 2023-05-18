6e795445c22687dcb6dcd613c6739a07f01f9d93
Sergio Veliz <>
2023-05-18 17:53:48 +0300

    initial

---
+++ meditation/meditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditationApp.swift
+//  meditation
+//
+//  Created by Sergio Veliz on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct meditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
